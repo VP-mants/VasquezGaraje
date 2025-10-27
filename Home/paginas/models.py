09d703267c09d273ab661442ca00f38501b07166
VP_mants <vicentepavez234@gmail.com>
2025-10-27 03:29:21 -0300

    Integracion de Base de datos en login y estructura para siguientes segmentos, definicion de login y registro de usuario

--- Home/paginas/models.py
+++ Home/paginas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+	cliente_id = models.AutoField(primary_key=True)
+	nombre_cliente = models.CharField(max_length=100)
+	apellido_cliente = models.CharField(max_length=100)
+	correo_cliente = models.EmailField(unique=True)
+	telefono_cliente = models.CharField(max_length=20)
+	contraseña_cliente = models.CharField(max_length=128)
+	es_admin = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f"{self.nombre_cliente} {self.apellido_cliente}"
+
+	class Meta:
+		db_table = 'CLIENTE'
