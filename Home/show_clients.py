7abfd37e37f9da504ff6bb6ab0b80dcaa66bbf08
VP_mants <vicentepavez234@gmail.com>
2025-11-12 02:29:24 -0300

    Estructura MVC Para el proyecto, todo ordenado y funcional

--- Home/show_clients.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-p = r'C:\Users\HP Omen\Desktop\VasquezGaraje\Home\DBVazquezGaraje.db'
-conn = sqlite3.connect(p)
-cur = conn.cursor()
-cur.execute("SELECT cliente_id, nombre_cliente, apellido_cliente, correo_cliente FROM CLIENTE LIMIT 20")
-rows = cur.fetchall()
-for r in rows:
-    print(r)
-conn.close()
---
+++ VasquezGaraje/Project_Config/show_clients.py
@@ -0,0 +1,11 @@
+import sqlite3
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+ p = BASE_DIR / 'Project_Config' / 'DBVasquezGaraje.db'
+conn = sqlite3.connect(p)
+cur = conn.cursor()
+cur.execute("SELECT cliente_id, nombre_cliente, apellido_cliente, correo_cliente FROM CLIENTE LIMIT 20")
+rows = cur.fetchall()
+for r in rows:
+    print(r)
+conn.close()
